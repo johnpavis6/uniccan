82b486699b3de12f78ed382397f509905fe7d650
Safiha <johnpavis6@gmail.com>
2019-03-30 22:30:18 +0530

    small changes

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,3 @@
-var templateContent = document.querySelector('#skill-set').content;
 var app = angular.module('uniccan', []);
 app.controller('profileCtrl', function ($scope, $http) {
     $scope.search = function () {
@@ -13,19 +12,5 @@ app.controller('profileCtrl', function ($scope, $http) {
             console.log(res.data);
             $scope.helpers = res.data;
         }, function (res) {});
-    }
-    $scope.filter = function () {
-        $('#skills').html('');
-        var q = $scope.q.trim();
-        if (!q.length) {
-            return;
-        }
-        var inputVal = new RegExp(q, 'i');
-        var set = Array.prototype.reduce.call(templateContent.cloneNode(true).children, function searchFilter(frag, item, i) {
-            if (inputVal.test(item.textContent) && frag.children.length < 6) frag.appendChild(item);
-            return frag;
-        }, document.createDocumentFragment());
-        console.log(set);
-        $('#skills').append(set);
     }
 });
